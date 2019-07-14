9df2bd2dcdd40d642b5ae576ec1f43dcb0f0c9ea
jesusmontano <jesusmontano96@gmail.com>
2019-07-13 17:43:34 -0700

    Add user validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,31 @@ const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get("/test", (req, res) => {
+    res.json({ msg: "This is the users route" })
+});
 
-
+router.get(
+    "./current",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        res.send(req.user);
+    }
+)
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -32,6 +51,42 @@ router.post('/register', (req, res) => {
                     })
                 })
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'This user does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            }
+                        )
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
         })
 })
 
