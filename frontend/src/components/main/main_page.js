86db3344082720fe979eb5cc315ed79d04b65085
jesusmontano <jesusmontano96@gmail.com>
2019-07-13 20:41:13 -0700

    Add placeholder button to main page component

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -6,6 +6,7 @@ class MainPage extends React.Component {
         return (
             <div>
                 <h1>BeastMode</h1>
+                <button>Generate my workout!</button>
                 <footer>
                     Copyright &copy; 2019 BeastMode
         </footer>
