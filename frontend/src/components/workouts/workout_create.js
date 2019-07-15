bf9c200e73f55bc876e8ca9eccc661a87c4df82d
Kevin <kevinkumar90@gmail.com>
2019-07-15 13:38:36 -0700

    Fix create workout form

--- frontend/src/components/workouts/workout_create.js
+++ frontend/src/components/workouts/workout_create.js
@@ -12,7 +12,9 @@ export default class WorkoutCreate extends React.Component {
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeCategory = this.handleChangeCategory.bind(this);
+        this.handleChangeFatigue = this.handleChangeFatigue.bind(this);
+        this.handleChangeEquipment = this.handleChangeEquipment.bind(this);
     }
 
   
@@ -20,13 +22,29 @@ export default class WorkoutCreate extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         
-        // this.props.composeWorkout(workout);
+        if (!this.state.category === "" && !this.state.fatigue === "" 
+        && this.state.equipment === "") {
+            // this.props.composeWorkout(workout);
+        }
+
+        
     }
 
     
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
+    handleChangeCategory(event) {
+        
+        this.setState({ category: event.target.value });
+    }
+
+    handleChangeFatigue(event) {
+        
+        this.setState({ fatigue: event.target.value });
+    }
+
+    handleChangeEquipment(event) {
+        
+        this.setState({ equipment: event.target.value });
     }
 
     render() {
@@ -38,7 +56,8 @@ export default class WorkoutCreate extends React.Component {
                     <div>
                         <label>
                             What do you want to work out?:
-                            <select value={this.state.category} onChange={this.handleChange}>
+                            <select value={this.state.category} onChange={this.handleChangeCategory}>
+                                {/* <option name="category" defaultValue=""></option> */}
                                 <option name="category" value="Arms">Arms</option>
                                 <option name="category" value="Shoulders">Shoulders</option>
                                 <option name="category" value="Chest">Chest</option>
@@ -52,9 +71,10 @@ export default class WorkoutCreate extends React.Component {
                         Great, let's work out our {this.state.category}!
                         <label>
                             What's your level of fatigue?
-                            <select value={this.state.fatigue} onChange={this.handleChange}>
+                            <select value={this.state.fatigue} onChange={this.handleChangeFatigue}>
+                                {/* <option name="fatigue" defaultValue=""></option> */}
                                 <option name="fatigue" value="0">Full of energy, not fatigued at all</option>
-                                <option name="fatigue" value="1"></option>
+                                <option name="fatigue" value="1">Neither</option>
                                 <option name="fatigue" value="2">Very fatigued, let's take it easy</option>
                             </select>
                         </label>
@@ -63,7 +83,8 @@ export default class WorkoutCreate extends React.Component {
                     <div>
                         <label>
                             Do you have access to gym equipment?
-                            <select value={this.state.equipment} onChange={this.handleChange}>
+                            <select value={this.state.equipment} onChange={this.handleChangeEquipment}>
+                                {/* <option name="equipment" defaultValue=""></option> */}
                                 <option name="equipment" value="yes">Yes</option>
                                 <option name="equipment" value="no">No</option>
                             </select>
@@ -72,7 +93,7 @@ export default class WorkoutCreate extends React.Component {
                 </form>
                 <br />
 
-                <WorkoutBox category={this.state.category} fatigue={this.state.fatigue} equipment={this.state.equipment} />
+                {/* <WorkoutBox category={this.state.category} fatigue={this.state.fatigue} equipment={this.state.equipment} /> */}
             </div>
         )
     }
